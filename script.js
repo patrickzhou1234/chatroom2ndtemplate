2aa0104bf74b10cd5a4252ecbc0863930cdff70a
patrickzhou1234 <83720283+patrickzhou1234@users.noreply.github.com>
2021-09-11 22:04:19 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+const socket = io();
+socket.emit("joined");
+
+socket.on("joined", () => {
+  msgs.innerHTML += "A user has joined. <br>";
+});
+
+socket.on("leave", () => {
+  console.log(name+" left");
+  msgs.innerHTML += "A user has left. <br>";
+});
+
+msginput = document.getElementById("msginput");
+username = document.getElementById("username");
+msgs = document.getElementById("msgs");
+
+function sendmsg() {
+  socket.emit('chatmsg', username.value, msginput.value);
+  msginput.value = "";
+}
+
+socket.on('chatmsg', function(user, msg) {
+  msgs.innerHTML += user+": "+msg+"<br>";
+});
+
+function welc() {
+  username.style.display = "none";
+  document.getElementById("submituser").style.display = "none";
+  msginput.style.display = "block";
+  document.getElementById("chatbtn").style.display = "block";
+  socket.emit('welc', username.value);
+}
+
+socket.on('welc', function(user) {
+  msgs.innerHTML += "Bot🤖: Welcome to the chat "+user+". <br>";
+});
+
+function checkinput(event, element) {
+  if (event.keyCode == "13") {
+    if (element == "msginput") {
+      sendmsg();
+    } else {
+      welc();
+    }
+  }
+}
